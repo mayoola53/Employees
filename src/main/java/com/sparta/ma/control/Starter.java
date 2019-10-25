519f97e3c1c458c232db81ad946c6b37529d856a
Michael Ayoola <mayoola53@yahoo.co.uk>
2019-10-25 11:22:34 +0100

    Initial commit

---
+++ src/main/java/com/sparta/ma/control/Starter.java
@@ -0,0 +1,13 @@
+package com.sparta.ma.control;
+
+/**
+ * Hello world!
+ *
+ */
+public class Starter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
